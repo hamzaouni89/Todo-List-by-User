feb23eddbaa094c1379869a8f2d0fa75043fc6aa
hamzainfo89 <hamzaouni@gmail.com>
2019-05-14 13:24:21 +0100

    update

--- frontTodo/src/app/todo.service.ts
+++ frontTodo/src/app/todo.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Http, Response, RequestOptions } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
-import { map } from 'rxjs/operators';
+import { map } from 'rxjs/operators/map';
 import 'rxjs/add/operator/filter';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/catch';
